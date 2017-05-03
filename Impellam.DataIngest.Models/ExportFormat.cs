b4b6d3fed40be563f43fe3bf6fb9b8fafee2b5cd
Ian Moores <imitconsultancy@gmail.com>
2017-05-03 16:22:22 +0100

    Added files to share with Team

---
+++ Impellam.DataIngest.Models/ExportFormat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impellam.DataIngest.Models
+{
+  public class ExportFormat : SourceData
+  {
+    public ExportFormat()
+    {
+      ExportDate = DateTime.Now;
+    }
+
+    public DateTime ExportDate { get; }
+
+    public bool Paid { get; set; } = false;
+    public DateTime? DatePaid { get; set; }
+    public static ExportFormat FromSourceFormat(SourceData input)
+    {
+      return new ExportFormat()
+      {
+        Id = input.Id,
+        DateWorked = input.DateWorked,
+        HoursWorked = input.HoursWorked,
+        PayDate = input.PayDate,
+        SourceName = input.SourceName
+      };
+
+    }
+
+
+
+  }
+}
---
+++ Impellam.DataIngest.Models/SourceData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Impellam.DataIngest.Models
+{
+  public class SourceData
+  {
+    public string SourceName { get; set; }
+    public DateTime PayDate { get; set; }
+    public double HoursWorked { get; set; }
+    public DateTime DateWorked { get; set; }
+    public string Id { get; set; }
+    public override string ToString()
+    {
+      return JsonConvert.SerializeObject(this);
+    }
+  }
+}
